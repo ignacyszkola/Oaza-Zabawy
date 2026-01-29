82c24f8578aa534aee19b196a6bd017f8f5a2a10
Wixerek <144936214+ignacyszkola@users.noreply.github.com>
2026-01-29 02:10:05 +0100

    Data

---
+++ Js/data.js
@@ -0,0 +1,28 @@
+// ===== ENUMY =====
+const Time = { LONG:"LONG", MEDIUM:"MEDIUM", SHORT:"SHORT", DEPENDS:"DEPENDS" };
+const Ppl = { ODB:"ODB", OND:"OND", ONZ:"ONZ", YOUNGER:"YOUNGER", OLDER:"OLDER", EVERYWHERE:"EVERWHERE" };
+
+// ===== LISTA ZABAW =====
+const zabawy = [
+  {name:"Polka", time:Time.SHORT, ppl:Ppl.EVERYWHERE},
+  {name:"Kiedy Umrę Kochanie", time:Time.SHORT, ppl:Ppl.EVERYWHERE},
+  {name:"Belgijka", time:Time.SHORT, ppl:Ppl.EVERYWHERE},
+  {name:"Menuet", time:Time.SHORT, ppl:Ppl.ONZ},
+  {name:"Tunak", time:Time.SHORT, ppl:Ppl.EVERYWHERE},
+  {name:"Church clap", time:Time.SHORT, ppl:Ppl.OLDER},
+  {name:"Oczko", time:Time.DEPENDS, ppl:Ppl.OLDER},
+  {name:"Miritmo", time:Time.MEDIUM, ppl:Ppl.OLDER},
+  {name:"Czołgi", time:Time.LONG, ppl:Ppl.OLDER},
+  {name:"Haczyki", time:Time.DEPENDS, ppl:Ppl.EVERYWHERE},
+  {name:"Samoloty", time:Time.SHORT, ppl:Ppl.EVERYWHERE},
+  {name:"Kto tak jak ja", time:Time.MEDIUM, ppl:Ppl.OLDER},
+  {name:"Psychiatryk", time:Time.LONG, ppl:Ppl.OLDER},
+  {name:"Dyrygent", time:Time.LONG, ppl:Ppl.EVERYWHERE},
+  {name:"Tunel", time:Time.LONG, ppl:Ppl.EVERYWHERE},
+  {name:"Eeej bejbe", time:Time.SHORT, ppl:Ppl.EVERYWHERE},
+  {name:"Pitonga", time:Time.MEDIUM, ppl:Ppl.EVERYWHERE},
+  {name:"Randka w ciemno", time:Time.LONG, ppl:Ppl.ONZ},
+  {name:"Macarena", time:Time.SHORT, ppl:Ppl.EVERYWHERE},
+  {name:"Pif paf", time:Time.LONG, ppl:Ppl.OLDER},
+  {name:"Skrzypek", time:Time.SHORT, ppl:Ppl.EVERYWHERE}
+];
